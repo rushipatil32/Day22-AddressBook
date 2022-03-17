1c18363559d6ff5aab90b86b71683433683982c1
Rushikesh Patil <rushipatil6632@gmail.com>
2022-03-17 12:42:17 +0530

    Added:ability to adding a contacts

--- src/com/blz/addressbook/AddressBook.java
+++ src/com/blz/addressbook/AddressBook.java
@@ -1,20 +1,52 @@
 package com.blz.addressbook;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AddressBook {
-	 private ArrayList<ContactPerson> contactBook ;
+    Scanner sc = new Scanner(System.in);
+    private ArrayList<ContactPerson> contactBook = new ArrayList<>();
 
-	    public AddressBook(ArrayList<ContactPerson> contactBook) {
-	        this.contactBook = contactBook;
-	    }
+    public ArrayList<ContactPerson> getContactBook() {
+        return contactBook;
+    }
 
-	    public ArrayList<ContactPerson> getContactBook() {
-	        return contactBook;
-	    }
+    public void setContactBook(ArrayList<ContactPerson> contactBook) {
+        this.contactBook = contactBook;
+    }
 
-	    public void setContactBook(ArrayList<ContactPerson> contactBook) {
-	        this.contactBook = contactBook;
-	    }
-	
+    public void addContacts() {
+        System.out.println("Enter the Contact details-->");
+        System.out.println("Add Contact");
+        System.out.println("Enter first name:");
+        String firstName = sc.next();
+        System.out.println("Enter last name");
+        String lastName = sc.next();
+        System.out.println("Enter city");
+        String city = sc.next();
+        System.out.println("Enter address");
+        String address = sc.next();
+        System.out.println("Enter state");
+        String state = sc.next();
+        System.out.println("Enter email");
+        String email = sc.next();
+        System.out.println("Enter Phone");
+        int phoneNumber = sc.nextInt();
+        System.out.println("Enter Zip");
+        int zip = sc.nextInt();
+        ContactPerson contact = new ContactPerson(firstName, lastName, address, state, city, email, phoneNumber, zip);
+        contactBook.add(contact);
+        display();
+    }
+
+    public void display() {
+        for (ContactPerson contacts : contactBook
+        ) {
+            System.out.println("==Contact Details==");
+            System.out.println("\nFirst Name: " + contacts.getFirstName() + "\t Last Name: " + contacts.getLastName()
+                    + "\n Address: " + contacts.getAddress() + "  " + contacts.getCity() + "  " + contacts.getState()
+                    + "\n Phone Number: " + contacts.getPhoneNumber() + "\tEmail:" + contacts.getEmail());
+
+        }
+    }
 }
--- src/com/blz/addressbook/AddressBookMain.java
+++ src/com/blz/addressbook/AddressBookMain.java
@@ -1,8 +1,10 @@
 package com.blz.addressbook;
 
 public class AddressBookMain {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("\n Welcome to Address Book System");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addContacts();
+        addressBook.display();
     }
 }
-
