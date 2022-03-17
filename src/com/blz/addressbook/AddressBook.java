b6e4da7260fa4b1eb702ea1faeecad3dd76b2cd8
Rushikesh Patil <rushipatil6632@gmail.com>
2022-03-17 12:48:53 +0530

    Added:ability to edit existing contact

--- src/com/blz/addressbook/AddressBook.java
+++ src/com/blz/addressbook/AddressBook.java
@@ -1,18 +1,16 @@
 package com.blz.addressbook;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class AddressBook {
     Scanner sc = new Scanner(System.in);
-    private ArrayList<ContactPerson> contactBook = new ArrayList<>();
+    private ContactPerson contactBook[];
+    private int numberOfConatcts = 0;
 
-    public ArrayList<ContactPerson> getContactBook() {
-        return contactBook;
-    }
-
-    public void setContactBook(ArrayList<ContactPerson> contactBook) {
-        this.contactBook = contactBook;
+    AddressBook() {
+        this.contactBook = new ContactPerson[20];
     }
 
     public void addContacts() {
@@ -34,19 +32,71 @@ public class AddressBook {
         int phoneNumber = sc.nextInt();
         System.out.println("Enter Zip");
         int zip = sc.nextInt();
-        ContactPerson contact = new ContactPerson(firstName, lastName, address, state, city, email, phoneNumber, zip);
-        contactBook.add(contact);
+        contactBook[numberOfConatcts] = new ContactPerson(firstName, lastName, address, state, city, email, phoneNumber, zip);
+        numberOfConatcts++;
+        display();
+    }
+
+    public void edit() {
+        System.out.println("enter the name to edit contact details");
+        String name = sc.next();
+        System.out.println("enter the choice to edit details");
+        System.out.println("1.First Name\\n2.Last Name\\n3.City\\n4.State\\n5.Zip Code\\n6.Phone\\n7.Email");
+        int choice = sc.nextInt();
+        int index = 0;
+        for (index = 0; index < numberOfConatcts; index++)
+            if (name.equals(contactBook[index].getFirstName())) {
+                System.out.println("name exists , now enter the new details");
+
+                break;
+            } else {
+                System.out.println("No contact found");
+                return;
+            }
+        switch (choice) {
+            case 1:
+                System.out.println("Enter new First Name");
+                String newFirstName = sc.next();
+                contactBook[index].setFirstName(newFirstName);
+                break;
+            case 2:
+                System.out.println("Enter new Last Name");
+                String newLastName = sc.next();
+                contactBook[index].setLastName(newLastName);
+                break;
+            case 3:
+                System.out.println("Enter new City");
+                String newCity = sc.next();
+                contactBook[index].setCity(newCity);
+                break;
+            case 4:
+                System.out.println("Enter new State");
+                String newState = sc.next();
+                contactBook[index].setState(newState);
+                break;
+            case 5:
+                System.out.println("Enter new State");
+                int newZip = sc.nextInt();
+                contactBook[index].setZip(newZip);
+                break;
+            case 6:
+                System.out.println("Enter new Phone Number");
+                int newPNumber = sc.nextInt();
+                contactBook[index].setPhoneNumber(newPNumber);
+                break;
+            case 7:
+                System.out.println("Enter new Email");
+                String newEmail = sc.next();
+                contactBook[index].setEmail(newEmail);
+                break;
+        }
         display();
     }
 
     public void display() {
-        for (ContactPerson contacts : contactBook
-        ) {
-            System.out.println("==Contact Details==");
-            System.out.println("\nFirst Name: " + contacts.getFirstName() + "\t Last Name: " + contacts.getLastName()
-                    + "\n Address: " + contacts.getAddress() + "  " + contacts.getCity() + "  " + contacts.getState()
-                    + "\n Phone Number: " + contacts.getPhoneNumber() + "\tEmail:" + contacts.getEmail());
-
+        for (int index = 0; index < numberOfConatcts; index++) {
+            System.out.println(contactBook[index]);
         }
     }
+
 }
--- src/com/blz/addressbook/AddressBookMain.java
+++ src/com/blz/addressbook/AddressBookMain.java
@@ -1,10 +1,24 @@
 package com.blz.addressbook;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println("\n Welcome to Address Book System");
         AddressBook addressBook = new AddressBook();
-        addressBook.addContacts();
-        addressBook.display();
+        System.out.println("Enter \n1 -> Add Contact \n2 -> Edit Existing Contact ");
+        switch (scanner.nextInt()) {
+            case 1:
+                addressBook.addContacts();
+                break;
+            case 2:
+                addressBook.edit();
+                break;
+            default:
+                System.out.println("Enter valid Number");
+
+        }
     }
 }
